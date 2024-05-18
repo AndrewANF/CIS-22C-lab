1ff18ad2c725198ef918d5adc0f161fd3cd36166
jynnieK <karahawke.io@gmail.com>
2024-05-18 16:56:02 -0700

    Update LinkNode.h
    Summed constructors into oneliners
    Wrote destructor (delete Currency obj)

--- LinkNode.h
+++ LinkNode.h
@@ -3,31 +3,12 @@
 class LinkNode
 {
 public:
-    
-    Currency* _data;
-    LinkNode* _next;
+	LinkNode() : _data(nullptr), _next(nullptr) {};
+	LinkNode(Currency* data) : _data(data), _next(nullptr) {};
+	LinkNode(Currency* data, LinkNode* next) : _data(data), _next(next) {};
+	~LinkNode() { delete _data; }
 
-    LinkNode();
-    LinkNode(Currency* data);
-    LinkNode(Currency* data, LinkNode* next);
-    ~LinkNode();
-
+	// private: // I wonder if the node needs private data + getter & setter?
+	Currency* _data;
+	LinkNode* _next;
 };
-
-LinkNode::LinkNode(){
-    _data = nullptr;
-    _next = nullptr;
-}
-LinkNode::LinkNode(Currency* data){
-    _data = data;
-    _next = nullptr;
-}
-LinkNode::LinkNode(Currency* data, LinkNode* next){
-    _data = data;
-    _next = next;
-}
-
-
-LinkNode::~LinkNode()
-{
-}
