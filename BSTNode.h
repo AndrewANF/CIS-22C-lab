170da6d40262357b562602a2f426606abbee8a3d
jynnieK <karahawke.io@gmail.com>
2024-06-01 00:00:09 -0700

    Update BSTNode.h

--- BSTNode.h
+++ BSTNode.h
@@ -1,27 +1,26 @@
 // Lab 4 - 6 June 2024
-// BSTNode.h
+// BST.h
 // Linhnam Le & Andrew Forsberg
 // This assignment demonstrates binary search trees
 
-#pragma once
 
-// BSTNode ADT
-// This code is templatized for any datatype
+// Binary search tree implementation
 
-//template <typename T>
+
 class BSTNode {
 public:
 	BSTNode() : _data(0), _left(nullptr), _right(nullptr) {};
+	BSTNode(Currency* obj) : _data(obj), _left(nullptr), _right(nullptr) {};
 	~BSTNode() {};
 
 	void setLeft(BSTNode* left) { _left = left; }
 	void setRight(BSTNode* right) { _right = right; }
 	BSTNode* left() const { return _left; }
 	BSTNode* right() const { return _right; }
-	Dollar* data() const { return _data; }
+	Currency* data() const { return _data; }
 
 private:
-	Dollar* _data;
-	BSTNode* _left;
-	BSTNode* _right;
+	Currency* _data;
+	BSTNode* _left = nullptr;
+	BSTNode* _right = nullptr;
 };
