cc87c375c26e786f4a91d3a2c504f0417ce7d0e1
jynnieK <karahawke.io@gmail.com>
2024-06-01 00:36:11 -0700

    Update lab4.cpp

--- lab4.cpp
+++ lab4.cpp
@@ -1,14 +1,19 @@
-// Lab 4 - 6 June 2024
+// Lab 4 - 1 June 2024
 // lab4.cpp
 // Linhnam Le & Andrew Forsberg
 // This assignment demonstrates binary search trees
 
 #include <iostream>
-#include "Currency.h"
-#include "Dollar.h"
 #include <vector>
+#include "BST.h"
 
 using namespace std;
+
+const int SIZE = 20;
+const double TEST_VALS[SIZE] = {
+	57.12, 23.44, 87.43, 68.99, 111.22, 44.55, 77.77, 18.36, 543.21, 20.21,
+	345.67, 36.18, 48.48, 101.00, 11.00, 21.00, 51.00, 1.00, 251.00, 151.00
+};
 
 int main()
 {
@@ -17,13 +22,13 @@ int main()
 	tree->inOrder();
 
 	vector<Currency*> currencies;
-	for (int i = 0; i < SIZE; i++) {
-		currencies.push_back(new Dollar(TEST_VALS[i]));
-	}
+	//for (int i = 0; i < SIZE; i++) {
+	//	currencies.push_back(new Dollar(TEST_VALS[i]));
+	//}
 
-	for (int i = 0; i < SIZE; i++) {
-		tree->insert(currencies.at(i));
-	}
+	//for (int i = 0; i < SIZE; i++) {
+	//	tree->insert(currencies.at(i));
+	//}
 	tree->inOrder();
 
 	cout << "Count: " << tree->count() << endl;
@@ -34,6 +39,7 @@ int main()
 
 	cout << "loading things" << endl;
 
-
 	return 0;
 }
+
+
