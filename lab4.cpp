cca04a4982e5ad1c6fe44b0a14bf7fa59983cad2
Andrew Nunez Forsberg <andrewanforsberg@gmail.com>
2024-06-01 22:56:48 -0700

    added add still need remove and seach to work

--- lab4.cpp
+++ lab4.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <vector>
 #include "BST.h"
+#include "Dollar.h"
 #include <sstream>
 #include <fstream>
 
@@ -43,8 +44,28 @@ void appendAndClose(std::string messege) {
     }
 
 
+}
+
+void traverse(BST* tree){
+
+  appendAndClose("\nBFS");
+	cout << "\nBreadth follows:" << endl;
+	tree->breadth();
+
+	appendAndClose("\ninOrder");
+	cout << "\ninOrder follows:" << endl;
+	tree->inOrder();
+
+	appendAndClose("\npreOrder");
+	cout << "\npreOrder follows:" << endl;
+	tree->preOrder();
+	
+	appendAndClose("\npostOrder");
+	cout << "\npostOrder follows:" << endl;
+	tree->postOrder();
 
 }
+
 const int SIZE = 20;
 const double TEST_VALS[SIZE] = {
 	57.12, 23.44, 87.43, 68.99, 111.22, 44.55, 77.77, 18.36, 543.21, 20.21,
@@ -73,40 +94,52 @@ int main()
 
 	/*********************************************/
 
-	// Four traversal methods. TODO: write to output log
+	// Four traversal methods. 
 
-	appendAndClose("\nBFS");
-	cout << "\nBreadth follows:" << endl;
-	tree->breadth();
-
-	appendAndClose("\ninOrder");
-	cout << "\ninOrder follows:" << endl;
-	tree->inOrder();
-
-	appendAndClose("\npreOrder");
-	cout << "\npreOrder follows:" << endl;
-	tree->preOrder();
-	
-	appendAndClose("\npostOrder");
-	cout << "\npostOrder follows:" << endl;
-	tree->postOrder();
-
+  traverse(tree);
 	/*********************************************/
 
-	string line;
-	do {
-		cout << "Commands: add, remove, search, exit" << endl;
-		cout << "For add/remove/search, enter a currency value, e.g. \"add 12.44\"" << endl;
 
-		getline(cin, line);
-		stringstream ss(line);
 
-		string operation;
-		string value;
+  while(true){
 
-		ss >> operation >> value;
+    //Get input
+    std::string input = "";
+    cout << "Commands: add, remove, search, exit" << endl;
+	  cout << "For add/remove/search, enter a currency value, e.g. \"add 12.44\"" << endl;
+    std::getline(std::cin, input);
+    
+    //Tokenize the input
+    std::istringstream iss(input);
+    std::vector<std::string> tokens;
+    std::string token;
+    
+    while(iss >> token){
+      tokens.push_back(token);
+    }
 
-	} while (1);
+	if (tokens[0] == "q" || tokens[0] == "quit"){return 0;} //quit program if q is entered
+
+  
+  if (tokens[0] == "add") {
+    Dollar* newNode = new Dollar(std::stof(tokens[1]));
+    tree->insert(newNode);
+  }
+
+  if (tokens[0] == "remove") {
+    Dollar* newNode = new Dollar(std::stof(tokens[1]));
+    tree->remove(newNode);
+  }
+ 
+    if (tokens[0] == "remove") {
+    Dollar* newNode = new Dollar(std::stof(tokens[1]));
+    tree->remove(newNode);
+  }
+ 
+  clearfile();
+  traverse(tree);
+
+}
 
 	return 0;
 }
