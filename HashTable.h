aade478cdf9577209034c3e902f3661a0fd9654f
jynnieK <karahawke.io@gmail.com>
2024-06-16 17:58:10 -0700

    Major fixes for code + cleanup and QA
    - Resolved pointer access exception (SEGFAULT) when searching for an item NOT on the table
    - Added input validation for search function; program will now refuse to accept characters found in input
    - Made hashFunc() private to HashTable class - remember hashing algo should be "secret"
    - Removed debugging cout statements
    - Added commentary on code where necessary
    - Cleaned up code for presentation

--- HashTable.h
+++ HashTable.h
@@ -1,50 +1,40 @@
+// Lab 5 - 16 June 2024
+// HashTable.h
+// Linhnam Le & Andrew Nunez Forsberg
+// This assignment demonstrates a Hashtable 
+
 #ifndef HASHTABLE
 #define HASHTABLE
-
 
 #include <vector>
 #include "Currency.h"
 
-class HashTable{
+class HashTable {
 
-  public:
-
-// Constructor to init hash table to size
-    HashTable(int size = 29);
-    
-    // Hash table Destuctor
+public:
+    HashTable(int size);
     ~HashTable();
-    
-    // 
-    Currency getValue(int index);
 
-    int search(Currency& searchValue);
+    Currency getValue(int index);       // TODO: returns a Currency object
+    int search(Currency& searchValue);  // Searches for a matching object and returns index
+    bool insert(Currency& newCurr);     // Return true on success
+    bool remove(Currency& targetCurr);  // Return true on success
+    void display(); // Prints hash table with indexes
 
-    bool insert(Currency& newCurr);
-
-    bool remove(Currency& targetCurr);
-
-    int getCollisions() { return _collisions ;}
-
+    // Getters follow
+    int getCollisions() { return _collisions; }
     double loadFactor() { return static_cast<double>(_currentSize) / _table.size(); }
 
-    void display();
 
-    int hashFunc(double amount); //TODO Change to Private Befor Upload
-
-  private:
-  
-    
+private:
+    // hash and find are kept private. See hashFunc() for hash algorithm
+    int hashFunc(double amount);
+    int findPosition(Currency& targetCurr); // TODO
 
     int _currentSize;
-
     int _collisions;
 
     std::vector<Currency*> _table;
-
-  
-    int findPosition(Currency& targetCurr);
-    
 };
 
 
