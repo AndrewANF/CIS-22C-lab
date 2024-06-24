53076c561be6256477dbdb77eb9013e316df7158
Andrew Nunez Forsberg <andrewanforsberg@gmail.com>
2024-06-23 21:17:47 -0700

    ran through a beautifier

--- MinHeap.h
+++ MinHeap.h
@@ -1,92 +1,86 @@
-
 //Min heap inharets from BinarySearchTree(BST) and Overrides search insert and delete
 #include "BST.h"
+
 #include "Currency.h"
+
 #include <cstddef>
+
 #include <iterator>
 
 
-class MinHeap: public BST{
- public:
-  
-  //Heapifys the tree
-  void minHeapify();
+class MinHeap: public BST {
+  public:
+
+    //Heapifys the tree
+    void minHeapify();
 
   //returns the minimum value in the tree
   //this will me the root
-  BSTNode* getMin();
+  BSTNode * getMin();
 
   //
-  BSTNode* search(Currency* target);
-  void insert(Currency* target);
+  BSTNode * search(Currency * target);
+  void insert(Currency * target);
   void remove();
-  
+
   int _heapSize = 0;
 
+  private:
 
- private:
+    //BSTNode* remove(BSTNode* node, Currency* target);
+    BSTNode * search(BSTNode * node, Currency * target);
+  BSTNode * insert(BSTNode * node, Currency * target);
+  void minHeapify(BSTNode * node);
 
-
-  //BSTNode* remove(BSTNode* node, Currency* target);
-  BSTNode* search(BSTNode* node, Currency* target);
-  BSTNode* insert(BSTNode* node, Currency* target);
-  void minHeapify(BSTNode* node);
- 
   //gets the next parent of the next 
   //node with < 2 children
-  BSTNode* getNextParent();
-  BSTNode* getNextParent(BSTNode* node);
+  BSTNode * getNextParent();
+  BSTNode * getNextParent(BSTNode * node);
 
-  BSTNode* getLastNode();
-  void deleteLastNode(BSTNode* last);
+  BSTNode * getLastNode();
+  void deleteLastNode(BSTNode * last);
 
   void deleteRoot();
 
-  void swap(BSTNode* a, BSTNode* b);
+  void swap(BSTNode * a, BSTNode * b);
 
-  BSTNode* getParent(BSTNode* node);
- 
- 
+  BSTNode * getParent(BSTNode * node);
 
 };
 
 // Returns the next parent to keep the 
 // heap a compleate tree
-inline BSTNode* MinHeap::getNextParent() {
+inline BSTNode * MinHeap::getNextParent() {
 
-  
   if (getRoot() == nullptr) {
     return nullptr;
   }
 
-
-  std::queue<BSTNode*> q;
+  std::queue < BSTNode * > q;
   q.push(getRoot());
 
   while (!q.empty()) {
-	  BSTNode* currentNode = q.front();
-	  q.pop();
+    BSTNode * currentNode = q.front();
+    q.pop();
 
-      if (currentNode->left() == nullptr || currentNode->right() == nullptr) {
-          return currentNode;
-      }
+    if (currentNode -> left() == nullptr || currentNode -> right() == nullptr) {
+      return currentNode;
+    }
 
-	  if (currentNode->left()) q.push(currentNode->left());
-	  if (currentNode->right()) q.push(currentNode->right());
+    if (currentNode -> left()) q.push(currentNode -> left());
+    if (currentNode -> right()) q.push(currentNode -> right());
   }
 
   return nullptr;
 }
 
-
-
 // makes the heap compleate and corretly formated
-inline void MinHeap::minHeapify(){
+inline void MinHeap::minHeapify() {
   if (getRoot()) {
 
     //checks if the left node is nullptr
     //if it is that means it has no children
-    if (getRoot()->left()) {
+    if (getRoot() -> left()) {
       minHeapify(getRoot());
     }
   }
@@ -94,64 +88,62 @@ inline void MinHeap::minHeapify(){
 
 //Used to enable recusive checking of the heap
 //I may want to change the way that swapping works swap objects not change values TODO?
-inline void MinHeap::minHeapify(BSTNode* node){
+inline void MinHeap::minHeapify(BSTNode * node) {
   if (!node) return;
 
-  if (node->left()) {
+  if (node -> left()) {
 
     //check left sub tree
-    if (node->left()) {
-      minHeapify(node->left());
+    if (node -> left()) {
+      minHeapify(node -> left());
     }
     //check right suntree
-    if (node->right()) {
-      minHeapify(node->right());
+    if (node -> right()) {
+      minHeapify(node -> right());
     }
-
 
     //if parent is greater then left children
     //swap
-    if (node->data()->isGreater(*node->left()->data() ) ) {
+    if (node -> data() -> isGreater( * node -> left() -> data())) {
 
       // parent node = temp
-      
-      int wholeTmp = node->data()->wholePart();
-      int fracTmp = node->data()->fractPart();
-      
+
+      int wholeTmp = node -> data() -> wholePart();
+      int fracTmp = node -> data() -> fractPart();
+
       // parentNode = leftChild
-      
-      node->data()->setWholePart(node->left()->data()->wholePart());
-      node->data()->setFractPart(node->left()->data()->fractPart());
-      
+
+      node -> data() -> setWholePart(node -> left() -> data() -> wholePart());
+      node -> data() -> setFractPart(node -> left() -> data() -> fractPart());
+
       // leftChild = temp 
-      
-      node->left()->data()->setWholePart(wholeTmp);
-      node->left()->data()->setFractPart(fracTmp);
+
+      node -> left() -> data() -> setWholePart(wholeTmp);
+      node -> left() -> data() -> setFractPart(fracTmp);
 
     }
   }
 
-
-  if (node->right()){
+  if (node -> right()) {
     // else if parent is greater then right child,
     //swap
-    if ( node->data()->isGreater(*node->right()->data()) ) {
-    
+    if (node -> data() -> isGreater( * node -> right() -> data())) {
+
       // parent node = temp
-      
-      int wholeTmp = node->data()->wholePart();
-      int fracTmp = node->data()->fractPart();
-      
+
+      int wholeTmp = node -> data() -> wholePart();
+      int fracTmp = node -> data() -> fractPart();
+
       // parentNode = rightChild
-      
-      node->data()->setWholePart(node->right()->data()->wholePart());
-      node->data()->setFractPart(node->right()->data()->fractPart());
-      
+
+      node -> data() -> setWholePart(node -> right() -> data() -> wholePart());
+      node -> data() -> setFractPart(node -> right() -> data() -> fractPart());
+
       // rightChild = temp 
-      
-      node->right()->data()->setWholePart(wholeTmp);
-      node->right()->data()->setFractPart(fracTmp);
-      
+
+      node -> right() -> data() -> setWholePart(wholeTmp);
+      node -> right() -> data() -> setFractPart(fracTmp);
+
     }
   }
 
@@ -159,30 +151,27 @@ inline void MinHeap::minHeapify(BSTNode* node){
 
 //inserts a targert Currency object in to the 
 //heap.
-inline void MinHeap::insert(Currency* target) {
-
-
+inline void MinHeap::insert(Currency * target) {
 
   //newNode will be the node
   //that is inserted
-  BSTNode* newNode = new BSTNode(target);
+  BSTNode * newNode = new BSTNode(target);
 
   if (getRoot() == nullptr) {
     setRoot(newNode);
-  }else{
+  } else {
     // parentNode will be used to 
     // deside where to put newNode
-    BSTNode* parentNode = getNextParent();
-    if (parentNode->left() == nullptr) {
-        parentNode->setLeft(newNode); 
+    BSTNode * parentNode = getNextParent();
+    if (parentNode -> left() == nullptr) {
+      parentNode -> setLeft(newNode);
     } else {
-        parentNode->setRight(newNode);
+      parentNode -> setRight(newNode);
     }
   }
   minHeapify();
 
 }
-
 
 /*
 searches the tree for a target currency
@@ -190,59 +179,55 @@ pre: Currency* targer
 post: na
 return: pointer to currency obj or nullptr if not found
 */
-inline BSTNode* MinHeap::search(Currency* target) {
-	return search(getRoot(), target);
+inline BSTNode * MinHeap::search(Currency * target) {
+  return search(getRoot(), target);
 }
 
-inline BSTNode* MinHeap::search(BSTNode* node, Currency* target) {
-	if (node) {
-		if (node->data()->isEqual(*target)) {
-			return node;
-		}
+inline BSTNode * MinHeap::search(BSTNode * node, Currency * target) {
+  if (node) {
+    if (node -> data() -> isEqual( * target)) {
+      return node;
+    }
     //tmp used to check if there are any 
     //matches for the sub trees
-    BSTNode* tmp = search(node->left(), target);
+    BSTNode * tmp = search(node -> left(), target);
 
     //checks left
-		if (tmp != nullptr) {
-			return tmp;
-		}
-    tmp = search(node->right(), target);
+    if (tmp != nullptr) {
+      return tmp;
+    }
+    tmp = search(node -> right(), target);
     //checks right
-		if(tmp != nullptr) {
-			return tmp;
-		}
-	}
-	return nullptr; // returns null if node not found
+    if (tmp != nullptr) {
+      return tmp;
+    }
+  }
+  return nullptr; // returns null if node not found
 }
 
+inline void MinHeap::swap(BSTNode * a, BSTNode * b) {
 
+  int wholeTmp = a -> data() -> wholePart();
+  int fracTmp = a -> data() -> fractPart();
 
+  // parentNode = rightChild
 
-inline void MinHeap::swap(BSTNode* a, BSTNode* b){
-      
-      int wholeTmp = a->data()->wholePart();
-      int fracTmp = a->data()->fractPart();
-      
-      // parentNode = rightChild
-      
-      a->data()->setWholePart(b->data()->wholePart());
-      a->data()->setFractPart(b->data()->fractPart());
-      
-      // rightChild = temp 
-      
-      b->data()->setWholePart(wholeTmp);
-      b->data()->setFractPart(fracTmp);
+  a -> data() -> setWholePart(b -> data() -> wholePart());
+  a -> data() -> setFractPart(b -> data() -> fractPart());
+
+  // rightChild = temp 
+
+  b -> data() -> setWholePart(wholeTmp);
+  b -> data() -> setFractPart(fracTmp);
 
 }
-
 
 inline void MinHeap::deleteRoot() {
 
   //checks for empty heap
-  if ( getRoot() == nullptr) return;
+  if (getRoot() == nullptr) return;
 
-  BSTNode* last = getLastNode();
+  BSTNode * last = getLastNode();
 
   //checks if there is only one node in heap
   if (last != getRoot()) {
@@ -256,91 +241,77 @@ inline void MinHeap::deleteRoot() {
 
 }
 
+inline BSTNode * MinHeap::getLastNode() {
 
-inline BSTNode* MinHeap::getLastNode() {
+  if (getRoot() == nullptr) return nullptr;
 
-if (getRoot() == nullptr) return nullptr;
+  std::queue < BSTNode * > q;
 
-    std::queue<BSTNode*> q;
-    
-    q.push(getRoot());
+  q.push(getRoot());
 
-    BSTNode* last = nullptr;
+  BSTNode * last = nullptr;
 
-    while (!q.empty()) {
+  while (!q.empty()) {
 
-        last = q.front();
-        q.pop();
+    last = q.front();
+    q.pop();
 
-        if (last->left()) q.push(last->left());
+    if (last -> left()) q.push(last -> left());
 
-        if (last->right()) q.push(last->right());
+    if (last -> right()) q.push(last -> right());
 
-    }
-    return last;
+  }
+  return last;
 }
 
+inline void MinHeap::deleteLastNode(BSTNode * last) {
 
+  if (getRoot() == nullptr) return;
 
-inline void MinHeap::deleteLastNode(BSTNode* last) {
+  std::queue < BSTNode * > q;
 
+  q.push(getRoot());
 
+  while (!q.empty()) {
 
-    if (getRoot() == nullptr) return;
+    BSTNode * node = q.front();
+    q.pop();
 
-    std::queue<BSTNode*> q;
+    if (node -> left() == last) {
 
-    q.push(getRoot());
-
-    while (!q.empty()) {
-
-        BSTNode* node = q.front();
-        q.pop();
-
-        if (node->left() == last) {
-
-            node->setLeft(nullptr);
-            delete last;
-            return;
-        } else if (node->right() == last) {
-            node->setRight(nullptr);
-            delete last;
-            return;
-        }
-
-        if (node->left()) q.push(node->left());
-        if (node->right()) q.push(node->right());
+      node -> setLeft(nullptr);
+      delete last;
+      return;
+    } else if (node -> right() == last) {
+      node -> setRight(nullptr);
+      delete last;
+      return;
     }
 
-
+    if (node -> left()) q.push(node -> left());
+    if (node -> right()) q.push(node -> right());
+  }
 
 }
 
+inline BSTNode * MinHeap::getParent(BSTNode * node) {
 
+  if (!getRoot() || getRoot() == node) return nullptr;
 
+  std::queue < BSTNode * > q;
 
-inline BSTNode* MinHeap::getParent(BSTNode* node) {
-
-    if (!getRoot() || getRoot() == node) return nullptr;
-
-    std::queue<BSTNode*> q;
-
-    q.push(getRoot());
-    while (!q.empty()) {
-        BSTNode* parent = q.front();
-        q.pop();
-        if (parent->left() == node || parent->right() == node) {
-            return parent;
-        }
-        if (parent->left()) q.push(parent->left());
-        if (parent->right()) q.push(parent->right());
+  q.push(getRoot());
+  while (!q.empty()) {
+    BSTNode * parent = q.front();
+    q.pop();
+    if (parent -> left() == node || parent -> right() == node) {
+      return parent;
     }
-    return nullptr;
+    if (parent -> left()) q.push(parent -> left());
+    if (parent -> right()) q.push(parent -> right());
+  }
+  return nullptr;
 }
-
-
-
-
 
 inline void MinHeap::remove() {
 
@@ -348,35 +319,26 @@ inline void MinHeap::remove() {
 
 }
 
-
-
-
-
-
-
 // Helper function to find the parent of a node
-inline BSTNode* MinHeap::getNextParent(BSTNode* node) {
-    if (getRoot() == nullptr || node == nullptr) {
-        return nullptr;
-    }
-
-    std::queue<BSTNode*> q;
-    q.push(getRoot());
-
-    while (!q.empty()) {
-        BSTNode* currentNode = q.front();
-        q.pop();
-
-        if (currentNode->left() == node || currentNode->right() == node) {
-            return currentNode;
-        }
-
-        if (currentNode->left()) q.push(currentNode->left());
-        if (currentNode->right()) q.push(currentNode->right());
-    }
-
+inline BSTNode * MinHeap::getNextParent(BSTNode * node) {
+  if (getRoot() == nullptr || node == nullptr) {
     return nullptr;
+  }
+
+  std::queue < BSTNode * > q;
+  q.push(getRoot());
+
+  while (!q.empty()) {
+    BSTNode * currentNode = q.front();
+    q.pop();
+
+    if (currentNode -> left() == node || currentNode -> right() == node) {
+      return currentNode;
+    }
+
+    if (currentNode -> left()) q.push(currentNode -> left());
+    if (currentNode -> right()) q.push(currentNode -> right());
+  }
+
+  return nullptr;
 }
-
-
- 
--- lab06.cpp
+++ lab06.cpp
@@ -1,66 +1,80 @@
-
 // Lab 6
 // lab6.cpp
 // Linhnam Le & Andrew Nunez Forsberg
 // This assignment demonstrates a Hashtable
 
-
-
 #include "Dollar.h"
+
 #include <iostream>
+
 #include <iterator>
-#include "MinHeap.h"    
+
+#include "MinHeap.h"
 
 const int SIZE = 20;
 const double TEST_VALS[SIZE] = {
-    57.12, 23.44, 87.43, 68.99, 111.22, 44.55, 77.77, 18.36, 543.21, 20.21,
-    345.67, 36.18, 48.48, 101.00, 11.00, 21.00, 51.00, 1.00, 251.00, 151.00
+  57.12,
+  23.44,
+  87.43,
+  68.99,
+  111.22,
+  44.55,
+  77.77,
+  18.36,
+  543.21,
+  20.21,
+  345.67,
+  36.18,
+  48.48,
+  101.00,
+  11.00,
+  21.00,
+  51.00,
+  1.00,
+  251.00,
+  151.00
 };
 
 int main() {
 
-    //create min Heap
-    MinHeap heap = MinHeap();
-    //insert test values from TEST_VALS
-    for (int i = 0; i < SIZE ; i++) {
-       // After 10th item print traversal methods
-      if (i == 10) {
-        std::cout << "Tenth Item added, Printing traversals: \n";
-        heap.print();
-      }
-      Dollar* newNode = new Dollar(TEST_VALS[i]);
-      heap.insert(newNode);
-
+  //create min Heap
+  MinHeap heap = MinHeap();
+  //insert test values from TEST_VALS
+  for (int i = 0; i < SIZE; i++) {
+    // After 10th item print traversal methods
+    if (i == 10) {
+      std::cout << "Tenth Item added, Printing traversals: \n";
+      heap.print();
     }
-     //After last item print traversal methods
-    std::cout << "\n\nFinal item added, Printing traversals:\n";
-    heap.print();
+    Dollar * newNode = new Dollar(TEST_VALS[i]);
+    heap.insert(newNode);
 
-    Dollar* searchTarget = new Dollar(1.0);
+  }
+  //After last item print traversal methods
+  std::cout << "\n\nFinal item added, Printing traversals:\n";
+  heap.print();
 
-    std::cout << "\n\nTesting Search: 1.0";
-    if (heap.search(searchTarget)) {
-      std::cout << "\nSearch target found";
-    }
-    else {
-      std::cout << "\nSearch target Not found";
-    }
+  Dollar * searchTarget = new Dollar(1.0);
 
-    std::cout << "\n";
-    heap.remove();
-    heap.print();
+  std::cout << "\n\nTesting Search: 1.0";
+  if (heap.search(searchTarget)) {
+    std::cout << "\nSearch target found";
+  } else {
+    std::cout << "\nSearch target Not found";
+  }
 
+  std::cout << "\n";
+  heap.remove();
+  heap.print();
 
-    std::cout << "\nTesting Search: 1.0";
-    if (heap.search(searchTarget)) {
-      std::cout << "\nSearch target found";
-    }
-    else {
-      std::cout << "\nSearch target Not found";
-    }
+  std::cout << "\nTesting Search: 1.0";
+  if (heap.search(searchTarget)) {
+    std::cout << "\nSearch target found";
+  } else {
+    std::cout << "\nSearch target Not found";
+  }
 
-
-    std::cout << std::endl;
-    //exit program
-    return 0;
+  std::cout << std::endl;
+  //exit program
+  return 0;
 }
